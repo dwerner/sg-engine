f8e444fca206f3dafd18647a4cdf7bd6ac021919
Daniel Werner <dan.werner@gmail.com>
2016-09-17 08:21:40 -0700

    adding game mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod net;
 pub mod physics;
 pub mod renderer;
+pub mod game;
 
 #[macro_use]
 extern crate vulkano;
@@ -9,13 +10,4 @@ extern crate winit;
 extern crate vulkano_win;
 extern crate glsl_to_spirv;
 extern crate vulkano_shaders;
-
-pub struct State {
-	blah: i32
-}
-
-#[no_mangle]
-pub extern "C" fn update(state: &mut State) {
-	//do stuff
-}
 
